748fb64990ebfbf539fa3c3e562ca4bd2b132624
Corbin Jensen <corbjens@gmail.com>
2017-03-20 19:28:04 -0600

    begin image slider work

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import Resume from '../assets/FrontEndDev-CorbinJensen.pdf'
 
 class Nav extends React.Component {
   render(){
     return (
-      <nav>
+      <nav id="Contact">
 
         <ul>
           <li><a href="tel:18776190818"><i className="icon-phone" aria-hidden="true"></i> 1 (877) 619-0818</a></li>
           <li><a href="mailto:c@corbinjensen.ca"><i className="icon-mail" aria-hidden="true"></i> Email</a></li>
-          <li><a href="./pdf/corbin-jensen-web-dev.compressed.pdf"><i className="icon-download" aria-hidden="true"></i> Download Resume</a></li>
+          <li><a href={Resume}><i className="icon-download" aria-hidden="true"></i> Download Resume</a></li>
           <li><a href="http://github.com/corbinjensen" target="_blank"><i className="icon-github" aria-hidden="true"></i> Github</a></li>
           <li><a href="https://www.linkedin.com/in/corbinjensen/" target="_blank"><i className="icon-linkedin" aria-hidden="true"></i> Linkedin</a></li>
         </ul>
