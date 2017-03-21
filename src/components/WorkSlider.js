0e379096ba136fcedf35a633122605fb67374014
Corbin Jensen <corbjens@gmail.com>
2017-03-20 19:40:00 -0600

    carosual was successful

--- src/components/WorkSlider.js
+++ src/components/WorkSlider.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import Slider from 'react-image-slider';
+
+import '../css/image-slider.css'
+
 import farmCommand from '../img/farmCommand.png'
 
 
 class WorkSlider extends React.Component {
   render(){
+    const images = [
+      '//corbinjensen.ca/portfolio/farmCommand.png',
+      '//placehold.it/600/3498db',
+      '//placehold.it/600/2ecc71',
+      '//placehold.it/600/9b59b6',
+      '//placehold.it/600/f1c40f',
+      '//placehold.it/600/e74c3c',
+      '//placehold.it/600/e67e22',
+    ];
     return (
       <div className="slider">
-        <img src={farmCommand} alt=""/>
+          <Slider images={images} isInfinite delay={5000}>
+          {images.map((image, key) => <div key={key}><img src={image} /></div>)}
+        </Slider>
       </div>
     )
   }
